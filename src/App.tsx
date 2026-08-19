c7a292e3b7a0657b723eae9cf8bcb00f35691c1a
Tema1313 <tolkouchebaivse@mail.ru>
2026-08-19 15:19:59 +0700

    feat: shadcn, tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button"
 
 function App() {
   return (
     <>
-      <div>Hello world</div>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button variant={"outline"}>Button</Button>
+      </div>
     </>
   )
 }
